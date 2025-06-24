f3c032ac6bab8f01a9f5370acd5917af4a50a68d
KRISHNAKUMARAP <krisharugam@gmail.com>
2025-06-24 16:45:05 +0530

    arun done the code

--- src/org/java/JavaOne.java
+++ src/org/java/JavaOne.java
@@ -3,11 +3,18 @@ package org.java;
 
 
 public class JavaOne {
+	
+	public static String s = "user defined";
+	
 	public static void main(String[] args) {
 		
 		int n = 3;
 		int n2 = 5;
 		System.out.println(n+n2);
+		
+		System.out.println(s);
+		
+		
 		
 	}
 
