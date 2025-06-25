12ef3f52a9faedbff8fc9741f52cf9259683941f
KRISHNAKUMARAP <krisharugam@gmail.com>
2025-06-25 11:59:38 +0530

    anbu create new class

---
+++ src/org/java/JavaTwo.java
@@ -0,0 +1,21 @@
+package org.java;
+
+
+public class JavaTwo {
+	
+	public static void main(String[] args) {
+		
+		
+		int a =7;
+		
+		for (int i = 0; i < a; i++) {
+			
+			System.out.println(i+1);
+			
+		}
+		
+	}
+	
+	
+	
+}
